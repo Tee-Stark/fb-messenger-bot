14a1ea704a7545a98a25752ccabb973cf3f8abf4
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-05-06 19:26:19 +0100

    add morgan production rule

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { NODE_ENV } = require('./config/constants');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -9,11 +10,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// require('./routes/webhook')(app);
+require('./routes/webhook')(app);
 
 if(NODE_ENV === 'development') {
-    const morgan = require('morgan');
     app.use(morgan('dev'));
+} else {
+    app.use(morgan('combined'));
 }
 
 module.exports = app;
