32f1cb3c824da41d8f07e5a45c70f4d6af4ad94a
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-05-07 13:29:52 +0100

    Add date difference calculator

---
+++ utils/birthday.js
@@ -0,0 +1,32 @@
+const { isDate } = require('./dateUtil');
+
+const nextBirthday = (birthday) => {
+    if(!isDate(birthday)) {
+        return -1;
+    }
+    // extract birthday date and month only
+    const dateArray = birthday.split('-');
+    let [year, month, day] = dateArray;
+    year = parseInt(year);
+    month = parseInt(month);
+    day = parseInt(day);
+    // check if birthday is today
+    const today = new Date();
+    if(today.getMonth() === month - 1 && today.getDate() === day) {
+        return 0;
+    }
+    // calculate next birthday
+    const nextBirthday = new Date(year, month - 1, day);
+    nextBirthday.setFullYear(today.getFullYear());
+    const diff = nextBirthday - today;
+    console.log(diff);
+    if(diff < 0) {
+        year = today.getFullYear() + 1;
+        nextBirthday.setFullYear(year);
+        console.log(nextBirthday)
+        return Math.ceil((nextBirthday - today) / (1000 * 60 * 60 * 24));
+    }
+    return Math.ceil(diff / (1000 * 60 * 60 * 24));
+}
+
+console.log(nextBirthday('2020-02-20'));
