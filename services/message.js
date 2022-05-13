176ec894a93a2401a12037e120c3854f5a8be7af
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-05-13 14:31:17 +0100

    Fix async await rule for message services

--- services/message.js
+++ services/message.js
@@ -1,17 +1,17 @@
 const Message = require('../models/message');
 
-const createNewMessage = (message) => {
+const createNewMessage = async (message) => {
     try {
         // check if user already exists
-        const user = Message.findOne({ userId: message.userId });
+        const user = await Message.findOne({ userId: message.userId });
         if(!user){
             // if user doesn't exist previously, create new user
-            const newMessage = Message.create(message);
+            const newMessage = await Message.create(message);
             return newMessage;
         } else {
             // else if user exists already, just add  the message's text to the user's messages
             if(typeof message === String) {
-                const updatedMessage = Message.findOneAndUpdate(
+                const updatedMessage = await Message.findOneAndUpdate(
                 { userId: message.userId }, 
                 { $push: { messages: message } }, 
                 { new: true }
@@ -30,15 +30,15 @@ const createNewMessage = (message) => {
  * function to get all messages
  * @param { 'all' | 'summary' } mode - the mode to get all messages
  */
-const getMessages = (mode) => {
+const getMessages = async (mode) => {
     try {
         if(mode === "all") {
             // find all messages and return only the messages
-            const messages = Message.find({}).select('messages -_id');
+            const messages = await Message.find({}).select('messages -_id');
             return messages;
         } else if(mode === "summary") {
             // find all messages and return @userId, @userName, and @messages
-            const messages = Message.find({}).select('-_id');
+            const messages = await Message.find({}).select('-_id');
             return messages;
         } else {
             throw new Error("invalid mode")
@@ -48,9 +48,9 @@ const getMessages = (mode) => {
     }
 }
 
-const getMessageById = (id) => {
+const getMessageById = async (id) => {
     try {
-        const message = Message.findById(id);
+        const message = await Message.findById(id);
         return message;
     } catch (err) {
         throw err;
